b57fa74e8ad303c08126dd1abfd29a01e51d6ade
juantobon20 <60023902+juantobon20@users.noreply.github.com>
2024-02-19 19:38:51 -0500

    Notification in iphone

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Juan Tobon on 19/02/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
